b0bfd34d04cfb99f4025adbea44430ec8f845e03
Carlsama <30857019+MrCarlsama@users.noreply.github.com>
2020-12-07 14:07:12 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,13 @@
 export const DATABASE_CONFIG = {
   host: 'localhost',
   port: 3306,
-  username: 'root',
-  password: 'caonima33',
+  username: '***********',
+  password: '***********',
 };
 
 export const OSS_CONFIG = {
-  region: 'oss-cn-guangzhou',
-  accessKeyId: 'LTAI4FzMGrkVcD9NsoftcCRs',
-  accessKeySecret: 'htsqcq0dmcamHvelgTCqaN91ouo2t5',
-  bucket: 'revue-starlight',
+  region: '***********',
+  accessKeyId: '***********',
+  accessKeySecret: '***********',
+  bucket: '***********',
 };
